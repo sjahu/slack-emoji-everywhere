96f2334c431a2fdae19998dc382196c7d6eb8a17
Stephen Humphries <s@shumphries.ca>
2023-01-13 02:39:44 -0500

    [WIP] Request site permissions one by one in options instead of using a global content script (TODO: inject scripts)

--- options.js
+++ options.js
@@ -26,3 +26,42 @@ browser.storage.local.get(["slackConfig", "selectedTeamId"]).then((item) => {
     }
   }
 });
+
+
+browser.permissions.getAll().then((permissions) => {
+  let manifest = browser.runtime.getManifest();
+  let optional_origins = permissions.origins.filter(
+    (origin) => !manifest.permissions.includes(origin) &&
+      !manifest.content_scripts.flatMap((content_script) => content_script.matches).includes(origin)
+  );
+
+  document.querySelector("#match-patterns").innerHTML = optional_origins.map((origin) => `
+    <li>
+      <div class="match-pattern"><code>${origin.replace("<all_urls>", "&lt;all_urls&gt;")}</code></div><input type="button" class="match-pattern-delete-button" value="Remove" data-origin="${origin}">
+    </li>
+  `).join("");
+
+  document.querySelectorAll(".match-pattern-delete-button").forEach((input) => {
+    input.addEventListener("click", () => {
+      browser.permissions.remove({ origins: [input.dataset.origin] }).then((success) => {
+        if (success) {
+          browser.runtime.reload();
+        }
+      });
+    })
+  });
+});
+
+
+document.querySelector("#match-pattern-add-button").addEventListener("click", () => {
+  try {
+    browser.permissions.request({ origins: [document.querySelector("#match-pattern-input").value] }).then((success) => {
+      if (success) {
+        document.querySelector("#match-pattern-input").value = "";
+        browser.runtime.reload();
+      }
+    });
+  } catch (error) {
+    alert("Invalid match pattern 😭\n\nSee https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Match_patterns");
+  }
+});
